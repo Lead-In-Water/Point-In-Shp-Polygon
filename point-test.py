8c875ba133fcc2bf5fd951565658348efcdde2f1
Dinesh Jagai <dinesh97@seas.upenn.edu>
2021-04-06 15:19:26 -0400

    edited requirements.txt

--- point-test.py
+++ point-test.py
@@ -8,10 +8,11 @@ path_to_aq_VA_shp = "shp files/aq_VA.shp"
 address_one = "337 North 40th Street,Philadelphia, PA, 19104" #not in VA_AQ
 address_two = "2222 Max Meadows, Virginia"  #in VA_AQ
 address_three = "Honaker, Virginia" #in VA_AQ
-address_four = "119 J C Hillman Dr, Honaker, VA 24260" #in VA_AQ
+address_four = "119 Hillman Dr, Honaker, VA 24260" #in VA_AQ
 address_five = "1135 Carthage St, Sanford, NC 27330"  #not in VA_AQ
 address_six = "11221 New Garden Rd, Honaker, VA 24260"  # in VA_AQ
 address_seven = "10300 New Garden Rd, Honaker, VA 24260"  # in VA_AQ
+
 print("address one coords: " + str(address_coordinates(address_one)))
 print(contains_address(address_one, path_to_aq_VA_shp))
 assert(contains_address(address_one, str(path_to_aq_VA_shp)) == False)
